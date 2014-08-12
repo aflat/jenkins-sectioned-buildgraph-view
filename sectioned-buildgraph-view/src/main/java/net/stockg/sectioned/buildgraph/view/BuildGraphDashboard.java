059b860427023fb641b3540a7273a4b92e2fc2a3
aflat <ghstock@gmail.com>
2014-08-12 18:11:29 -0400

    some configuration, lots of copied code, getting somewhere, but still a NEWB

--- sectioned-buildgraph-view/src/main/java/net/stockg/sectioned/buildgraph/view/BuildGraphDashboard.java
+++ sectioned-buildgraph-view/src/main/java/net/stockg/sectioned/buildgraph/view/BuildGraphDashboard.java
@@ -7,15 +7,22 @@
 package net.stockg.sectioned.buildgraph.view;
 
 import hudson.Extension;
+import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
 import hudson.model.Item;
 import hudson.model.TopLevelItem;
 import hudson.model.View;
 import hudson.model.ViewDescriptor;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -33,10 +40,25 @@ public class BuildGraphDashboard  extends View {
     public BuildGraphDashboard(String name) {
             super(name);
     }
+    
     @Override
     public Collection<TopLevelItem> getItems() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TopLevelItem> result = new ArrayList<TopLevelItem>();
+//        Set<AbstractProject> projects = new HashSet<AbstractProject>();
+//        
+//        for (AbstractProject project : projects) {
+//            Collection<AbstractProject<?, ?>> downstreamProjects = ProjectUtil.getAllDownstreamProjects(project).values();
+//            for (AbstractProject<?, ?> abstractProject : downstreamProjects) {
+//                result.add(getItem(abstractProject.getName()));
+//            }
+//        }
+//
+        return result;
     }
+//    @Override
+//    public Collection<TopLevelItem> getItems() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
 
     @Override
     public boolean contains(TopLevelItem tli) {
@@ -50,12 +72,19 @@ public class BuildGraphDashboard  extends View {
 
     @Override
     protected void submit(StaplerRequest sr) throws IOException, ServletException, Descriptor.FormException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        sr.bindJSON(this, sr.getSubmittedForm());
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Item doCreateItem(StaplerRequest sr, StaplerResponse sr1) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Item item = Jenkins.getInstance().doCreateItem(sr, sr1);
+		if (item != null) {
+			//jobColumns.add(new JobColumn(item.getName(), null, false, true));
+			owner.save();
+		}
+		return item;
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     @Extension
     public static final class DescriptorImpl extends ViewDescriptor {
